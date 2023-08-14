65cfc3df35f83a5e7b42f875d0fee35b1176f870
t-hirata74 <t.hirata.0463@gmail.com>
2023-08-14 12:26:55 +0900

    コメント追記

--- api/main.py
+++ api/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+# @ で始まるこの部分を、Pythonでは デコレータ と呼びます。
+# Javaのアノテーションと似た形式ですが、Pythonのデコレータは、関数を修飾し、関数に新たな機能を追加します。
 @app.get("/hello")
 async def hello():
     return {"message": "hello world!"}
