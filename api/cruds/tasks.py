40bf84ea5415a3f84b08b10534484fa7b3c65b80
t-hirata74 <t.hirata.0463@gmail.com>
2023-08-14 15:54:57 +0900

    コメント追加

--- api/cruds/tasks.py
+++ api/cruds/tasks.py
@@ -9,6 +9,8 @@ import api.schemas.task as task_schema
 # 4. DB上のデータを元にTaskインスタンス task を更新する（この場合、作成したレコードの id を取得する）
 # 5. 作成したDBモデルを返却する
 async def create_task(
+    # async def は関数が非同期処理を行うことができる、 「コルーチン関数」であるということを表します。
+    # await では、非同期処理、ここではDBへの接続（IO処理）が発生するため、「待ち時間が発生するような処理をしますよ」ということを示しています
     db: AsyncSession, task_create: task_schema.TaskCreate
 ) -> task_model.Task:
     task = task_model.Task(**task_create.dict())
