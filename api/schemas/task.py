e279168cd863e0a96944cb6e3b9161949943c0e9
t-hirata74 <t.hirata.0463@gmail.com>
2023-08-14 14:05:31 +0900

    TaskCreateを追加

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -2,8 +2,10 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
+class TaskCreate(BaseModel):
+    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
 
-class Task(BaseModel):
+class Task(BaseModel): # BaseModel はFastAPIのスキーマモデルであることを表すので、このクラスを継承して Task クラスを作成しています。
     id: int
     title: Optional[str] = Field(None, example="クリーニングを取りに行く")
     done: bool = Field(False, description="完了フラグ")
