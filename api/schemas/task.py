3bda85020a4c4d69db3ec7e66545728b71ac78d0
t-hirata74 <t.hirata.0463@gmail.com>
2023-08-14 14:11:01 +0900

    TaskBaseの追記漏れ

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -2,6 +2,9 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
+class TaskBase(BaseModel):
+    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
+
 class TaskCreate(TaskBase):
     pass
 
