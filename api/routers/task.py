1be4b7a8f7d37f7678ae4f99242bc587a3d96426
t-hirata74 <t.hirata.0463@gmail.com>
2023-08-14 16:46:13 +0900

    getとpostのアクションを設定

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,6 +1,10 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+import api.cruds.task as task_crud
+from api.db import get_db
 
 import api.schemas.task as task_schema
 
@@ -8,13 +12,15 @@ router = APIRouter()
 
 
 @router.get("/tasks", response_model=List[task_schema.Task])
-async def list_tasks():
-    return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
+async def list_tasks(db: AsyncSession = Depends(get_db)):
+    return await task_crud.get_tasks_with_done(db)
 
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+async def create_task(
+    task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
+):
+    return await task_crud.create_task(db, task_body)
 
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
