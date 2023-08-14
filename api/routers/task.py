b7b5be401aedabd629a07b4a3713c9ecdd6123d3
t-hirata74 <t.hirata.0463@gmail.com>
2023-08-14 14:35:47 +0900

    各CRUD定義のrouter追加

--- api/routers/task.py
+++ api/routers/task.py
@@ -12,16 +12,16 @@ async def list_tasks():
     return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
 
 
-@router.post("/tasks")
-async def create_task():
-    pass  # pass は「何もしない文」を表します。
+@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+async def create_task(task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
 
 
-@router.put("/tasks/{task_id}")
-async def update_task():
-    pass  # pass は「何もしない文」を表します。
+@router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
+async def update_task(task_id: int, task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
 
 
-@router.delete("/tasks/{task_id}")
-async def delete_task():
-    pass  # pass は「何もしない文」を表します。
+@router.delete("/tasks/{task_id}", response_model=None)
+async def delete_task(task_id: int):
+    return
